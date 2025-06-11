50c97cc6018d180cdfcb16c77f19de70022e4ef5
SAYAN02-DEV <sayan2003.dev@gmail.com>
2025-06-11 17:01:33 +0530

    Update main.cpp

--- software/esp32_code/main.cpp
+++ software/esp32_code/main.cpp
@@ -1,5 +1,38 @@
-//
-// Created by sayan on 6/11/25.
-//
+#include <WiFi.h>
+#include <ESP32Servo.h>
 
-#include "main.h"
+#define WIFI_SSID "Wokwi-GUEST"
+#define WIFI_PASSWORD ""
+
+#define LED 2
+#define SERVO_PIN 13
+
+Servo myServo;
+
+void InitWiFi() {
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected!");
+}
+
+void setup() {
+    Serial.begin(115200);
+    InitWiFi();
+
+    pinMode(LED, OUTPUT);
+    myServo.attach(SERVO_PIN);
+    myServo.write(0);
+}
+
+void loop() {
+    digitalWrite(LED, HIGH);
+    myServo.write(90);
+    delay(1000);
+    myServo.write(0);
+    digitalWrite(LED, LOW);
+    delay(1000);
+}
